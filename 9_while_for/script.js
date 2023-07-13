3f6ebfd519e659a8bb9f92ea1923b4749434b36e
Felipe Queiroz <felipe.de.a.queiroz@outlook.com>
2023-07-13 15:22:42 -0300

    Loop structures

---
+++ 9_while_for/script.js
@@ -0,0 +1,19 @@
+//While
+let x = 10;
+
+while (x > 0) {
+	console.log(x);
+	x--;
+}
+
+//Do While
+let y = 100;
+do{
+	console.log(y/2);
+	y = y - 5;
+} while (y >= 0);
+
+//For
+for(let i = 2; i < 100; i=i*2){
+	console.log(i)
+}
