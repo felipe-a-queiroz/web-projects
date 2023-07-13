cbb2e45c4abd942866f5e9524fc193420a72405e
Felipe Queiroz <felipe.de.a.queiroz@outlook.com>
2023-07-13 11:49:46 -0300

    Control structures

---
+++ 8_if_else/script.js
@@ -0,0 +1,12 @@
+let firstVar = 5;
+let secondVar = 3;
+
+if (firstVar + secondVar == 3) {
+	console.log('The result is 3')
+} else if(firstVar == 4){
+	console.log('First variable is 4');
+} else if(secondVar == 3){
+	console.log('Second variable is 3');
+} else {
+	console.log('None of the above');
+}
